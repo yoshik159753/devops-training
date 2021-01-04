c93c0297f7f20e8e473353bf4f369419f4e272db
Yoshi K <kai.outsider.777+git@gmail.com>
2021-01-04 22:33:09 +0900

    演習 contact ページの追加

---
+++ frontend/app/pages/contact.js
@@ -0,0 +1,19 @@
+import MyHead from "../components/myhead";
+
+const Contact = () => {
+  return (
+    <div>
+      <MyHead title="Contact" />
+
+      <main>
+        <h1>Contact</h1>
+        <p>
+          Contact the Ruby on Rails Tutorial about the sample app at the
+          <a href="https://railstutorial.jp/contact">contact page</a>.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default Contact;
