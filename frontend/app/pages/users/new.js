26a70a021844ab11f3173630621b761dc3cd4a88
Yoshi K <kai.outsider.777+git@gmail.com>
2021-01-15 22:53:34 +0900

    5.4.1 Usersコントローラ

---
+++ frontend/app/pages/users/new.js
@@ -0,0 +1,12 @@
+import Layout from "../../components/layout";
+
+const Users = () => {
+  return (
+    <Layout>
+      <h1>Users#new</h1>
+      <p>Find me in app/views/users/new.html.erb</p>
+    </Layout>
+  );
+};
+
+export default Users;
