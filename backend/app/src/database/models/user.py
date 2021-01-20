2cf0b2c539f283ecfa8c87f026d06dd4483cdc43
Yoshi K <kai.outsider.777+git@gmail.com>
2021-01-20 22:21:04 +0900

    6.2.2 存在性を検証する

--- backend/app/src/database/models/user.py
+++ backend/app/src/database/models/user.py
@@ -1,7 +1,32 @@
+from jsonschema import ValidationError, validate
+
+
 class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
 
     def is_valid(self):
+        try:
+            schema = {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "minLength": 1,
+                    },
+                    "email": {
+                        "type": "string",
+                        "minLength": 1,
+                    },
+                },
+                "required": ["name", "email"]
+            }
+            properties = {
+                "name": self.name.strip(),
+                "email": self.email.strip(),
+            }
+            validate(properties, schema)
+        except ValidationError:
+            return False
         return True
--- backend/app/tests/database/models/test_user.py
+++ backend/app/tests/database/models/test_user.py
@@ -3,7 +3,7 @@ import pytest
 from src.database.models.user import User
 
 
-@pytest.fixture(scope="module", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def user():
     user = User(name="Example User", email="user@example.com")
     yield(user)
@@ -13,3 +13,31 @@ def test_should_be_valid(user):
     """ User が有効かテスト
     """
     assert user.is_valid()
+
+
+@pytest.mark.parametrize(
+    "name", [
+        pytest.param(""),
+        pytest.param("   "),
+        pytest.param("　　　"),
+        pytest.param(" 　 　 　"),
+    ])
+def test_name_should_be_present(user, name):
+    """ User の name が空文字、空白のみの場合は無効であること
+    """
+    user.name = name
+    assert not user.is_valid()
+
+
+@pytest.mark.parametrize(
+    "email", [
+        pytest.param(""),
+        pytest.param("   "),
+        pytest.param("　　　"),
+        pytest.param(" 　 　 　"),
+    ])
+def test_email_should_be_present(user, email):
+    """ User の email が空文字、空白のみの場合は無効であること
+    """
+    user.email = email
+    assert not user.is_valid()
