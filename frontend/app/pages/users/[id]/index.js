b82ee4ed1be861e2214b21fbdfd158ea332accd4
Yoshi K <kai.outsider.777+git@gmail.com>
2021-02-02 22:31:42 +0900

    7.1.4 Gravatar画像とサイドバー

--- frontend/app/pages/users/[id]/index.js
+++ frontend/app/pages/users/[id]/index.js
@@ -1,12 +1,23 @@
+import crypto from "crypto";
 import Layout from "../../../components/layout";
 import getUserById from "../../../actions";
 
 const User = (props) => {
   const user = props.user;
+  const hashByEmail = crypto.createHash("md5").update(user.email).digest("hex");
+  const gravatarUrl = `https://secure.gravatar.com/avatar/#${hashByEmail}`;
 
   return (
-    <Layout pageTitle="User">
-      {user.id}, {user.name}, {user.email}, {user.created_at}, {user.updated_at}
+    <Layout pageTitle={user.name}>
+      <aside className="col-md-4">
+        <section className="user_info">
+          <h1>
+            <img src={gravatarUrl} alt={user.name} className="gravatar" />
+            {user.name}
+          </h1>
+        </section>
+      </aside>
+      {/* {user.id}, {user.name}, {user.email}, {user.created_at}, {user.updated_at} */}
     </Layout>
   );
 };
