daf43bf160c9696237f4644717e16f0ad7e1583c
Yoshi K <kai.outsider.777+git@gmail.com>
2021-02-03 22:03:09 +0900

    7.2.1 form_forを使用する

--- frontend/app/pages/signup.js
+++ frontend/app/pages/signup.js
@@ -4,7 +4,58 @@ const Signup = () => {
   return (
     <Layout pageTitle="Sign up">
       <h1>Sign up</h1>
-      <p>This will be a signup page for new users.</p>
+
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            value=""
+            name="name"
+            type="text"
+            className="form-control"
+            id="name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            value=""
+            name="email"
+            type="email"
+            className="form-control"
+            id="email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            value=""
+            name="password"
+            type="password"
+            className="form-control"
+            id="password"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="passwordConfirmation">Confirmation</label>
+          <input
+            value=""
+            name="passwordConfirmation"
+            type="password"
+            className="form-control"
+            id="passwordConfirmation"
+          />
+        </div>
+        <div className="form-group">
+          <button
+            onClick="#"
+            type="button"
+            className="btn btn-primary form-control"
+          >
+            Create my account
+          </button>
+        </div>
+      </form>
     </Layout>
   );
 };
