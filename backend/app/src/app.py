6500542c2f6dba40e4042761d8653e4dc961fdc3
Yoshi K <kai.outsider.777+git@gmail.com>
2021-02-04 22:14:00 +0900

    7.3.2 Strong Parameters

--- backend/app/src/app.py
+++ backend/app/src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 
 from src.routes.routes import initialize_routes
 
 app = Flask(__name__)
 
+CORS(app)
 api = Api(app)
 
 
